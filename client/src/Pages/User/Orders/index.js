920a09d9a0d84c2a3ee3f536b5a3c4e2b9554964
Will Mundy <TheWillMundy@users.noreply.github.com>
2020-06-12 18:12:46 -0500

    Added styling to user past orders page

--- client/src/Pages/User/Orders/index.js
+++ client/src/Pages/User/Orders/index.js
@@ -1,86 +1,121 @@
-import React from 'react';
-import { useQuery, gql, useMutation } from '@apollo/client';
-import { useHistory } from 'react-router';
+import React, { useState } from "react";
+import Modal from "react-modal";
+import { useQuery, gql, useMutation } from "@apollo/client";
+import { useHistory } from "react-router";
+import "./order.css";
+
+Modal.bind("#app");
 
 const GET_PAST_ORDERS = gql`
-    query GetOrders {
-        orderMany( filter: { OR: [{ fulfillment: Ready }, { fulfillment: Cancelled }, { fulfillment: Placed }, { fulfillment: Preparing }] } ) {
-            _id
-            __typename
-            vendor {
-                name
-            }
-            items {
-                product {
-                    name
-                }
-            }
-            user {
-                netid
-            }
-            fulfillment
-        }
-    }
+	query GetOrders {
+		orderMany(
+			filter: {
+				OR: [
+					{ fulfillment: Ready }
+					{ fulfillment: Cancelled }
+					{ fulfillment: Placed }
+					{ fulfillment: Preparing }
+				]
+			}
+		) {
+			_id
+			__typename
+			vendor {
+				name
+			}
+			items {
+				product {
+					name
+				}
+			}
+			user {
+				netid
+			}
+			fulfillment
+		}
+	}
 `;
 
 const CANCEL_ORDER = gql`
-    mutation CancelOrder($_id: MongoID!) {
-        orderUpdateOne(filter: { _id: $_id } , record: { fulfillment: Cancelled } ){
-            record {
-                _id
-                __typename
-                fulfillment
-            }
-        }
-    }
-`
+	mutation CancelOrder($_id: MongoID!) {
+		orderUpdateOne(
+			filter: { _id: $_id }
+			record: { fulfillment: Cancelled }
+		) {
+			record {
+				_id
+				__typename
+				fulfillment
+			}
+		}
+	}
+`;
+
+const OrderItemList = ({ items }) => {
+	return (
+		<div>
+			<p>Order Items: </p>
+			{items.map((item) => (
+				<p>{item.product.name}</p>
+			))}
+		</div>
+	);
+};
 
 const OrderDetail = ({ order }) => {
     const { _id, items, vendor, user } = order;
+    const [modalOpen, setModalOpen] = useState(false);
 
-    const [cancelOrder, ] = useMutation(
-        CANCEL_ORDER,
-    );
+    const openModal = () => setModalOpen(true);
+    const closeModal = () => setModalOpen(false);
 
-    const handleCancelOrder = () => {
-        cancelOrder({ variables: { _id: _id } });
-    };
+	const [cancelOrder] = useMutation(CANCEL_ORDER);
 
-    return (
-        <div>
-            <h2>{order.fulfillment}</h2>
-            <p>{vendor.name}</p>
-            <p>{_id}</p>
-            <div>
-                <p>Order Items: </p>
-                {items.map(item => <p>{item.product.name}</p>)}
-            </div>
-            { order.fulfillment == "Placed" ? <button onClick={handleCancelOrder}>Cancel Order</button> : null }
-        </div>
-    )
-}
+	const handleCancelOrder = () => {
+		cancelOrder({ variables: { _id: _id } });
+	};
 
-const OrderList = ({ }) => {
-    const history = useHistory();
+	return (
+		<div className="ordercard">
+			<h2>{order.fulfillment}</h2>
+			<p>{vendor.name}</p>
+            <button onClick={openModal}>Show Order Items</button>
+			{order.fulfillment == "Placed" ? (
+				<button onClick={handleCancelOrder}>Cancel Order</button>
+			) : null}
+            <Modal
+            className="modal"
+            isOpen={modalOpen}
+            onRequestClose={closeModal}
+            >
+                <OrderItemList items={items} />
+            </Modal>
+		</div>
+	);
+};
 
-    const { data: orderData, loading: orderLoading, error: orderError } = useQuery(
-        GET_PAST_ORDERS,
-        { fetchPolicy: "network-only" }
-    ); 
+const OrderList = ({}) => {
+	const history = useHistory();
 
-    if (orderError) return <p>Errors...</p>;
-    if (orderLoading) return <p>Loading...</p>
-    if (!orderData) return (<p>No data...</p>);
+	const {
+		data: orderData,
+		loading: orderLoading,
+		error: orderError,
+	} = useQuery(GET_PAST_ORDERS, { fetchPolicy: "network-only" });
 
-    const orders = orderData.orderMany;
+	if (orderError) return <p>Errors...</p>;
+	if (orderLoading) return <p>Loading...</p>;
+	if (!orderData) return <p>No data...</p>;
 
-    return (
-        <div>
-            {orders.map(order => {
-                return (<OrderDetail order={order} />);
-            })}
-        </div>
-    );
-}
+	const orders = orderData.orderMany;
+
+	return (
+		<div className="orderlist">
+			{orders.map((order) => {
+				return <OrderDetail order={order} />;
+			})}
+		</div>
+	);
+};
 
 export default OrderList;

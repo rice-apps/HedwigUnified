c059261ae842ee3a767983ea5d841a65b97b0141
Yaning Hu <helenahu@users.noreply.github.com>
2020-07-11 21:47:32 -0500

    put your commit msg here

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import { Routes } from './components/Routes';
 import Header from './components/Header';
-
 function App() {
     return (
         <React.Fragment>

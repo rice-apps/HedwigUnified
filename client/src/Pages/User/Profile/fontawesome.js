994e430a5883a912c792ba30a215e415e89fa5b6
alexisnic01as <65870703+alexisnic01as@users.noreply.github.com>
2020-08-01 15:37:21 -0500

    Adding Profile Screen (#15)
    * adding simple user sign up page
    
    * adding simple user sign up page
    
    * adding profile screen
    
    * Feature/alexis (#16)
    
    * Fixed Profile data bug with gql
    
    * Removed unnecessary print statements
    
    Co-authored-by: Will Mundy <TheWillMundy@users.noreply.github.com>

---
+++ client/src/Pages/User/Profile/fontawesome.js
@@ -0,0 +1,17 @@
+// import the library
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+import { faPhone, faStarAndCrescent ,faReceipt, faHandsHelping, faArrowLeft, faEnvelope, faPaperPlane, faQuestionCircle, faEdit, faChevronRight} from "@fortawesome/free-solid-svg-icons";
+
+library.add(
+    faEnvelope,
+    faPaperPlane,
+    faQuestionCircle,
+    faPhone,
+    faReceipt,
+    faHandsHelping,
+    faArrowLeft,
+    faEdit,
+    faChevronRight,
+    faStarAndCrescent,
+)

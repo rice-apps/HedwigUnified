5b64a54837a14a185ec9e39351e8ffd092983211
Victor Song <vms2@rice.edu>
2020-08-01 19:00:50 -0500

    Added more descriptions for types

--- api/src/models/ProductModel.js
+++ api/src/models/ProductModel.js
@@ -7,10 +7,24 @@ const ProductInterfaceTC = sc.createInterfaceTC({
     name: "Product",
     description: "The base product interface for the common data model",
     fields: {
-        name: GraphQLNonNull(GraphQLString),
-        description: GraphQLNonNull(GraphQLString),
-        dataSource: () => DataSourceEnumTC,
-        merchant: GraphQLNonNull(GraphQLString), // TODO: Add merchant data type
+        name: {
+            type: GraphQLNonNull(GraphQLString),
+            description: "The vendor's name for the product",
+        },
+        description: {
+            type: GraphQLNonNull(GraphQLString),
+            description: "The vendor's description of the product",
+        },
+        dataSource: {
+            type: () => DataSourceEnumTC,
+            description:
+                "The data source for this product. Must be one of SQUARE, SHOPIFY, or EXCEL",
+        },
+        merchant: {
+            type: GraphQLNonNull(GraphQLString),
+            description:
+                "Must be a merchant supported by Hedwig; check the list before using this field.",
+        }, // TODO: Add merchant data type
     },
 });
 
@@ -19,9 +33,20 @@ const ItemVariantTC = sc
         name: "ItemVariant",
         description: "A variant of an existing Item",
         fields: {
-            dataSourceId: GraphQLNonNull(GraphQLString),
-            parentItemId: GraphQLNonNull(GraphQLString),
-            price: GraphQLNonNull(MoneyTC.getType()),
+            dataSourceId: {
+                type: GraphQLNonNull(GraphQLString),
+                description:
+                    "The ID of the item variant in the data source. Up to developer to verify correctness.",
+            },
+            parentItemId: {
+                type: GraphQLNonNull(GraphQLString),
+                description:
+                    "The ID of the item of which this is a variant. May not be applicable for all data sources.",
+            },
+            price: {
+                type: GraphQLNonNull(MoneyTC.getType()),
+                description: "The base price of this variant.",
+            },
         },
     })
     .addInterfaces([ProductInterfaceTC]);
@@ -31,8 +56,15 @@ const ItemModifierTC = sc
         name: "ItemModifier",
         description: "A modifier for Items",
         fields: {
-            dataSourceId: GraphQLNonNull(GraphQLString),
-            price: GraphQLNonNull(MoneyTC.getType()),
+            dataSourceId: {
+                type: GraphQLNonNull(GraphQLString),
+                description:
+                    "The ID of the item modifier in the data source. Up to developer to verify correctness.",
+            },
+            price: {
+                type: GraphQLNonNull(MoneyTC.getType()),
+                description: "The base price of this variant.",
+            },
         },
     })
     .addInterfaces([ProductInterfaceTC]);
@@ -42,9 +74,19 @@ const ItemTC = sc
         name: "Item",
         description: "An item in the common data model",
         fields: {
-            dataSourceId: GraphQLNonNull(GraphQLString),
-            variants: GraphQLList(ItemVariantTC.getType()),
-            modifiers: GraphQLList(ItemModifierTC.getType()),
+            dataSourceId: {
+                type: GraphQLNonNull(GraphQLString),
+                description:
+                    "The ID of the item in the data source. Up to developer to verify correctness.",
+            },
+            variants: {
+                type: GraphQLList(ItemVariantTC.getType()),
+                description: "A list of variants of this item",
+            },
+            modifiers: {
+                type: GraphQLList(ItemModifierTC.getType()),
+                description: "A list of modifiers application to this item",
+            },
         },
     })
     .addInterfaces([ProductInterfaceTC]);

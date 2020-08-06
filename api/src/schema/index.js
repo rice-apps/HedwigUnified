3340dadf847de4f6195da9cad2087cd305e08fa2
Will Mundy <TheWillMundy@users.noreply.github.com>
2020-08-05 19:58:11 -0500

    Added setup code for VendorTC hours resolver

--- api/src/schema/index.js
+++ api/src/schema/index.js
@@ -3,15 +3,18 @@ import { PaymentMutations } from "./PaymentSchema";
 import { UserQueries } from "./UserSchema";
 import { ItemQueries } from "./ProductSchema";
 import { OrderMutations } from "./OrderSchema";
+import { VendorQueries, VendorMutations } from "./VendorSchema";
 
 sc.Query.addFields({
     ...UserQueries,
     ...ItemQueries,
+    ...VendorQueries
 });
 
 sc.Mutation.addFields({
     ...PaymentMutations,
     ...OrderMutations,
+    ...VendorMutations
 });
 
 const Schema = sc.buildSchema();

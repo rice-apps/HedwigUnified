c242eed83c4ba4ba64309806165fdcab62722c80
spoken-angus <angusjelinek@gmail.com>
2020-09-27 14:02:30 -0500

    feature/order-completion and order-failure pages

--- client/src/Pages/User/Cart/Payments.js
+++ client/src/Pages/User/Cart/Payments.js
@@ -1,7 +1,7 @@
 import React, { Component, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Payments = () => {
-
     return (
         <div>
             Hello
--- client/src/Pages/User/Confirmation/index.js
+++ client/src/Pages/User/Confirmation/index.js
@@ -1,37 +1,16 @@
-import React, {
-  ToolBar,
-  Container,
-  Fragment,
-  useContext,
-  useEffect
-} from 'react'
+import React, {useState} from 'react'
 import { useQuery, gql } from '@apollo/client'
 import { useHistory, useLocation } from 'react-router'
-import {
-  AppBar,
-  Grid,
-  Toolbar,
-  BottomNavigation,
-  Divider
-} from '@material-ui/core'
-import withStyles from '@material-ui/core/styles/withStyles'
 import './confirmation.css'
 // import '../../fonts/style.css'
 //fontawesome imports
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {
-  faDoorOpen,
-  faDoorClosed,
-  faShoppingCart,
-  faReceipt,
-  faMapPin
-} from '@fortawesome/free-solid-svg-icons'
-import { PickupDropdown } from '../../../components/PickupDropdown'
-
+import { faMapPin } from '@fortawesome/free-solid-svg-icons'
 import { useNavigate } from 'react-router-dom'
 import { ReactComponent as FailureSVG } from './alert-circle.svg';
 import { ReactComponent as ConfirmationSVG } from './check-circle.svg';
+import {cartItems} from '../../../apollo';
+
 // const GET_VENDORS_QUERY = gql`
 //     query VendorList {
 //         vendorMany {
@@ -43,6 +22,8 @@ import { ReactComponent as ConfirmationSVG } from './check-circle.svg';
 
 const Confirmation = ({ classes }) => {
   // const { data, loading, error } = useQuery(GET_VENDORS_QUERY)
+  const [availability, setAvailability] = useState(true)
+  
   const vendor = {
     name: 'East West Tea',
     message: '“When your order is ready, wait in the tables in Sammy’s, and we will bring your order out to you”',
@@ -61,32 +42,57 @@ const Confirmation = ({ classes }) => {
   // if (error) return <p>Error...</p>
   // if (loading) return <p>Loading...</p>
   // if (!data) return <p>No data...</p>
-  let vendorId = '5ecf473e41ccf22523280c3c'
-  let dataSourceIdTest = 'WCCTHDOMDN564YCZYPJP5DF3'
+  // let vendorId = '5ecf473e41ccf22523280c3c'
+  // let dataSourceIdTest = 'WCCTHDOMDN564YCZYPJP5DF3'
+  const GET_CATALOG = gql`
+  query GET_CATALOG($vendorName: String!){
+    getCatalog(vendor: $vendorName, dataSource: SQUARE) {
+    dataSourceId
+    variants {
+      dataSourceId
+    }
+    isAvailable
+  }
+}
+`;
 
-  // const GET_CATALOG = gql`
-  //   query GET_CATALOG($item: String!){
-  //     getCatalog(dataSource: SQUARE, vendor: $item){
-  //       name
-  //       dataSourceId
-  //       isAvailable
-  //     }
+const ewteaName = "East West Tea"
+const { data: catalog, loading: catalogLoading, error: catalogError } = useQuery(GET_CATALOG, { variables: { vendorName: ewteaName } });
+
+if (catalogLoading) { return(<p>loading...</p>)}
+if (catalogError) { return (<p>Error.</p>)}
+console.log(catalog)
+  // const FETCH_AVAILABILITY = gql`
+  //   query FETCH_AVAILśBILITY($productId: String!){
+  //     getAvailability(productId: $productId)
   //   }
   // `;
 
-  // const { data: catalog, loading: catalogLoading, error: catalogError } = useQuery(GET_CATALOG, { variables: { item: vendorId } });
-
-  const FETCH_AVAILABILITY = gql`
-    query FETCH_AVAILABILITY($productId: String!){
-      getAvailability(productId: $productId)
+  // const { data: availability, loading, error } = useQuery(FETCH_AVAILABILITY, { variables: { productId: /*catalog.dataSourceId*/dataSourceIdTest } });
+  // console.log(availability)
+  const cartIds = cartItems().map (item => item.Id)
+  catalog.getCatalog.forEach(element => {
+    // if not, order not confirmed page
+    if (cartIds.includes(element.dataSourceId)) {
+      if (element.isAvailable !== true) {
+        setAvailability(false)
+      }
     }
-  `;
+    element.variants.forEach(variant => {
+      if (cartIds.includes(variant.dataSourceId)) {
+      if (element.isAvailable !== true) {
+        setAvailability(false)
+        }
+      }
+    })
+  });
 
-  const { data: availability, loading, error } = useQuery(FETCH_AVAILABILITY, { variables: { venID: /*catalog.dataSourceId*/dataSourceIdTest } });
 
+  
+  cartItems().map (item => console.log(item))
+  console.log(cartItems()); 
   // how do we get the current item in cart? 
   // right now we hard coded vendor id and item
-  // also availability is undefined
   function renderFailure() {
     return (
       <div className='mainDiv'>
@@ -124,11 +130,11 @@ const Confirmation = ({ classes }) => {
     </div>
     )
   }
-  
+  if (catalogLoading) return <p>Loading...</p>
   return (
     <div>
     
-    {(availability === false) ? renderConfirmation(): renderFailure()}
+    {availability ? renderConfirmation(): renderFailure()}
 
     </div>
   )

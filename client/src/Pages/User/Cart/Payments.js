6e845f3afc09e6f80893d077de08ea1691283c11
Ananya Vaidya <akv4@rice.edu>
2020-10-04 15:02:09 -0500

    button color changes for payment options

--- client/src/Pages/User/Cart/Payments.js
+++ client/src/Pages/User/Cart/Payments.js
@@ -4,17 +4,24 @@ import styled, { css } from 'styled-components';
 // import visa from 'payment-icons/min/flat/visa.svg';
 import { FaCreditCard, FaBriefcase } from 'react-icons/fa';
 import { BiIdCard } from 'react-icons/bi';
+import zIndex from "@material-ui/core/styles/zIndex";
 
 const Payments = () => {
 
-    const [bgColor, setBgColor] = useState([false, false, false]);
+    // The index of the button that is clicked (0, 1, or 2), if no button is clicked the index is 3
+    const [activeButton, setActiveButton] = useState(3);
+    const [activeFooter, setActiveFooter] = useState(0);
+
+    // colors
+    const bgColors = ['white', '#cf5734'];
+    const fontColors = ['#595858', 'white'];
 
     const Title = styled.text`
             margin-top: 110px;
             margin-bottom: 20px;
             font-family: "adobe-clean", sans-serif;
             font-size: 25px;
-            color: #595858;
+            color: #595858; 
             font-weight: lighter;
         `;
 
@@ -35,7 +42,7 @@ const Payments = () => {
             font-weight: 500;
             color: #595858;
             text-align: center;
-            background-color: ${props => (props.selected ? 'red' : 'white')};
+            &:hover {background-color: 'red'}
         `;
 
     const Grid = styled.div`
@@ -58,35 +65,46 @@ const Payments = () => {
             border-width: 1px;
             padding: 25px 0;
             font-size: 25px;
-        `;
+            color: ${fontColors[activeFooter]};
+            background-color: ${bgColors[activeFooter]};
+    `;
+
+    // Changes background color of a button if its selected
+    const renderBgColor = (index) => {
+        return (index === activeButton ? '#cf5734' : 'white');
+    };
+
+    // Changes font color of a button if its selected
+    const renderFontColor = (index) => {
+        return (index === activeButton ? 'white' : '#595858');
+    };
+
+    // Displays the payment option buttons
+    const renderButtons = () => {
+        const icons = [<BiIdCard size={40}></BiIdCard>, <FaCreditCard size={32}></FaCreditCard>, <FaBriefcase size={32}></FaBriefcase>]
+        const values = ["Tetra", "Credit Card", "Cohen Club Card"]
+
+        return (
+            icons.map((icon, index) =>
+                <Row>
+                    <Button variant="outlined" onClick={() => activeButton !== index ? setActiveButton(index) : setActiveButton(3)} style={{ backgroundColor: renderBgColor(index), color: renderFontColor(index) }}>{icons[index]} {values[index]}</Button>
+                </Row >
+
+            )
+        )
+    }
 
 
     return (
-
-        // <ButtonsCol>
-        //     <Title>Payment Method</Title>
-        //     <Button variant="outlined">Tetra</Button>
-        //     <Button variant="outlined">Credit Card</Button>
-        //     <Button variant="outlined">Cohen Club Card</Button>
-        // </ButtonsCol>
-        <div>
+        < div >
             <Grid>
                 <Row>
                     <Title>Payment Method</Title>
                 </Row>
-                <Row>
-                    <Button variant="outlined" onClick={() => setBgColor('red')}><BiIdCard size={40}></BiIdCard>Tetra</Button>
-
-                </Row>
-                <Row>
-                    <Button variant="outlined"><FaCreditCard size={32}></FaCreditCard> Credit Card</Button>
-                </Row>
-                <Row>
-                    <Button variant="outlined"><FaBriefcase size={32}></FaBriefcase> Cohen Club Card</Button>
-                </Row>
+                {renderButtons()}
             </Grid>
-            <Footer>Next</Footer>
-        </div>
+            <Footer onClick={() => activeFooter === 0 ? setActiveFooter(1) : setActiveFooter(0)}>Next</Footer>
+        </div >
     )
 };
 

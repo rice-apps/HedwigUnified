c5fbdc60f4ed491026ddcdbdae83e058d993afe5
sst7 <sst7@rice.edu>
2020-10-11 19:55:38 -0500

    Created interactive menu

--- client/src/Pages/Vendor/VendorComponents/VendorHeaderComponents/VendorHeader.js
+++ client/src/Pages/Vendor/VendorComponents/VendorHeaderComponents/VendorHeader.js
@@ -113,7 +113,7 @@ function MakeLogoutPopup() {
   return (
     <IconContext.Provider value={{style: {marginRight: "7px"}}}>
     <VendorHeaderWrapper>
-      <StyledUserDisplayWrapper onClick={toggleLogoutScreen}>
+      <StyledUserDisplayWrapper>
         <UserText>Newton Huynh</UserText>
         <FaUserCircle style={{ fontSize: "30px", marginLeft: "1vw" }} />
       </StyledUserDisplayWrapper>
@@ -124,7 +124,6 @@ function MakeLogoutPopup() {
         <div hidden>{setInterval(UpdateTime, 1000)}</div>
 
       </DateTimeDisplayWrapper>
-      {showLogout ? <MakeLogoutPopup /> : null}
     </VendorHeaderWrapper>
     </IconContext.Provider>
   );

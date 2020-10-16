355c553d8471091a9c45ca897d13bdb5b6989306
Newton Huynh <55329846+newton-huynh@users.noreply.github.com>
2020-10-16 08:10:02 -0500

    Change font of bottom app bar (#68)

--- client/src/Pages/User/Vendors/BottomAppBar.js
+++ client/src/Pages/User/Vendors/BottomAppBar.js
@@ -1,53 +1,66 @@
-import React from 'react'
-import styled from 'styled-components'
-import {FaShoppingCart} from'react-icons/fa'
-import {MdReceipt} from 'react-icons/md'
-import { useNavigate } from 'react-router-dom'
-import {AiFillHome} from 'react-icons/ai'
+import React from "react";
+import styled from "styled-components";
+import { FaShoppingCart } from "react-icons/fa";
+import { MdReceipt } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
+import { AiFillHome } from "react-icons/ai";
+import RalewayFont from "./../../../fonts/Raleway/RalewayFont.js";
 
 const BottomNavigationWrapper = styled.div`
-position:fixed;
-bottom:0px;
-height:10vh;
-width:100vw;
-background-color:white;
-display: grid;
-grid-template-columns: 1fr 1fr;
-grid-template-rows: 1fr;
-border-top:1px solid darkgrey;
-align-items: center;
-justify-content: center;
-z-index:2;
-`
-
-
+  position: fixed;
+  bottom: 0px;
+  height: 10vh;
+  width: 100vw;
+  background-color: white;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr;
+  border-top: 1px solid darkgrey;
+  align-items: center;
+  justify-content: center;
+  z-index: 2;
+`;
 
 const BottomNavigationItem = styled.div`
-height:100%;
-width: 100%;
-background-color:white;
-display:flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-cursor: pointer;
-&&:active{
-  transform: scale(1.02);
-  background-color:#FAC8BB;
-}
+  height: 100%;
+  width: 100%;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  font-family: "Raleway";
+  font-weight:500;
+  &&:active {
+    transform: scale(1.001);
+    background-color: #fac8bb;
+  }
+`;
 
-`
+const BottomNavigationText = styled.div`
+  font-family: "Raleway";
+ 
+`;
 
-
-function BottomAppBar () {
-  const navigate = useNavigate()
+function BottomAppBar() {
+  const navigate = useNavigate();
   return (
-
     <BottomNavigationWrapper>
-      <BottomNavigationItem style={{borderRight:"0.1px solid #D0D0D0"}} onClick={() => navigate('/eat')}><AiFillHome/>Home</BottomNavigationItem>
-      <BottomNavigationItem  onClick={()=> navigate('/eat/cohen/cart')}><FaShoppingCart/>Cart</BottomNavigationItem>
+      <RalewayFont/>
+      <BottomNavigationItem
+        style={{ borderRight: "0.1px solid #D0D0D0" }}
+        onClick={() => navigate("/eat")}
+      >
+        <AiFillHome />
+        <BottomNavigationText>Home</BottomNavigationText>
+      </BottomNavigationItem>
+      <BottomNavigationItem onClick={() => navigate("/eat/cohen/cart")}>
+        <FaShoppingCart />
+        <BottomNavigationText>Cart</BottomNavigationText>
+      </BottomNavigationItem>
     </BottomNavigationWrapper>
-  )
+  );
 }
 
-export default BottomAppBar
+export default BottomAppBar;

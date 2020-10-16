6d52a087bb38c8903b31cc07393034f3787ffe36
Newton Huynh <nth8@rice.edu>
2020-10-16 09:42:03 -0500

    Fix cart page styling errors, add functional back button to buyer frontend

--- client/src/Pages/User/Vendors/BuyerHeader.js
+++ client/src/Pages/User/Vendors/BuyerHeader.js
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import { FaUserCircle } from "react-icons/fa";
 import HedwigLogoFinal from "./../../../images/HedwigLogoFinal.png";
 import RalewayFont from "./../../../fonts/Raleway/RalewayFont";
+import { IoMdArrowRoundBack } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
 
 const HeaderWrapper = styled.div`
   position: fixed;
@@ -16,32 +18,53 @@ const HeaderWrapper = styled.div`
   grid-template-rows: 1fr;
   background-color: white;
   z-index: 1;
-  padding-top:1vh;
+  padding-top: 1vh;
 `;
 
 const HedwigLogo = styled.img`
   height: 4.5vh;
   width: 4.5vh;
-  margin-right:5px;
+  margin-right: 5px;
   margin-top: 0l5vh;
 `;
 
 const HedwigWrapper = styled.div`
   font-family: "Raleway";
   font-weight: 500;
-  display:flex;
+  display: flex;
   justify-content: center;
   align-items: center;
-  color:#DB6142;
+  color: #db6142;
 `;
 
-function BuyerHeader() {
+function BuyerHeader(props) {
+  const navigate = useNavigate();
+
   return (
     <HeaderWrapper>
       <RalewayFont />
       <HedwigWrapper>
+        {props.showBackButton ? (
+          <IoMdArrowRoundBack
+            onClick={() => navigate(props.backLink)}
+            style={{
+              position: "fixed",
+              left: "22px",
+              fontSize: "25px",
+              verticalAlign: "middle",
+              cursor: "pointer"
+            }}
+          />
+        ) : null}
         <HedwigLogo src={HedwigLogoFinal} /> Hedwig
-        <FaUserCircle style={{position:"fixed", right:"22px", fontSize:'25px', verticalAlign: "middle"}}/>
+        <FaUserCircle
+          style={{
+            position: "fixed",
+            right: "22px",
+            fontSize: "25px",
+            verticalAlign: "middle"
+          }}
+        />
       </HedwigWrapper>
     </HeaderWrapper>
   );

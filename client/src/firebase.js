8d20f2d18ddfff400f56407e3b422a3cd9c019e2
Lorraine-Lyu <yl191@rice.edu>
2020-10-21 14:41:14 -0500

    adds styled files

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,7 +2,11 @@
 import { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN } from './config'
 
 // This import loads the firebase namespace along with all its type information.
+<<<<<<< HEAD
 import firebase from 'firebase/app'
+=======
+import * as firebase from 'firebase/app'
+>>>>>>> adds styled files
 
 // These imports load individual services into the firebase namespace.
 import 'firebase/auth'

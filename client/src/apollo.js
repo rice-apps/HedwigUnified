51d467f06891465dab207e892d641597bb696328
Newton Huynh <nth8@rice.edu>
2020-12-14 15:38:57 -0600

    Delete yarn lock and change package lock

--- client/src/apollo.js
+++ client/src/apollo.js
@@ -4,69 +4,69 @@ import {
   HttpLink,
   InMemoryCache,
   split,
-  gql,
-} from "@apollo/client";
-import { getMainDefinition } from "@apollo/client/utilities";
-import { setContext } from "@apollo/link-context";
+  gql
+} from '@apollo/client'
+import { getMainDefinition } from '@apollo/client/utilities'
+import { setContext } from '@apollo/link-context'
 
 // Apollo Subscriptions Setup
-import { WebSocketLink } from "@apollo/link-ws";
-import { GRAPHQL_URL, GRAPHQL_WS_URL, SERVICE_URL } from "./config";
+import { WebSocketLink } from '@apollo/link-ws'
+import { GRAPHQL_URL, GRAPHQL_WS_URL, SERVICE_URL } from './config'
 
-import { makeVar } from "@apollo/client";
+import { makeVar } from '@apollo/client'
 
-console.log(GRAPHQL_URL);
-console.log(GRAPHQL_WS_URL);
-console.log(SERVICE_URL);
+console.log(GRAPHQL_URL)
+console.log(GRAPHQL_WS_URL)
+console.log(SERVICE_URL)
 
-export const cartItems = makeVar([]);
+export const cartItems = makeVar([])
 // export const orderSummary = makeVar({vendor: null})
-export const orderSummary = makeVar({ vendor: null, time: null });
+export const orderSummary = makeVar({ vendor: null, time: null })
 
-export const userProfile = makeVar([]);
+export const userProfile = makeVar([])
 
 // Wraps our requests with a token if one exists
 // Copied from: https://www.apollographql.com/docs/react/v3.0-beta/networking/authentication/
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem('token')
   // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    },
-  };
-});
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
 
 // HTTP Backend Link
 const httpLink = new HttpLink({
-  uri: GRAPHQL_URL,
-});
+  uri: GRAPHQL_URL
+})
 
 // WebSocket Backend Link
 const wsLink = new WebSocketLink({
   uri: GRAPHQL_WS_URL,
   options: {
-    reconnect: true,
-  },
-});
+    reconnect: true
+  }
+})
 
 // Uses wsLink for subscriptions, httpLink for queries & mutations (everything else)
 const splitLink = split(
   ({ query }) => {
-    const definition = getMainDefinition(query);
+    const definition = getMainDefinition(query)
     return (
-      definition.kind === "OperationDefinition" &&
-      definition.operation === "subscription"
-    );
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    )
   },
   wsLink,
   httpLink
-);
+)
 
 // Setup cache
-const cache = new InMemoryCache();
+const cache = new InMemoryCache()
 
 // Initialize Client
 export const client = new ApolloClient({
@@ -75,25 +75,25 @@ export const client = new ApolloClient({
       Query: {
         fields: {
           cartItems: {
-            read() {
-              return cartItems();
-            },
-          },
-        },
-      },
-    },
+            read () {
+              return cartItems()
+            }
+          }
+        }
+      }
+    }
   }),
-  link: authLink.concat(splitLink),
-});
+  link: authLink.concat(splitLink)
+})
 
 // Initial local state
 const initialState = {
   service: SERVICE_URL,
   user: {
     recentUpdate: false,
-    _id: "",
-  },
-};
+    _id: ''
+  }
+}
 
 // Initialize cache with a state
 cache.writeQuery({
@@ -106,5 +106,5 @@ cache.writeQuery({
       }
     }
   `,
-  data: initialState,
-});
+  data: initialState
+})

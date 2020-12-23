9781af846c9e1de4752afa591128d828e6b6c337
Vinay Tummarakota <55774665+vinaytummarakota@users.noreply.github.com>
2020-12-22 19:46:41 -0600

    Changed front-end to reflect new hours (#86)
    * Fixed vendor hours with new isClosed boolean
    
    * removed commas
    
    * Fixed backend so Sunday is first in vendor hours
    
    * Changed front-end to reflect new vendor hours
    
    * Added VendorCard.js changes
    
    * Merge conflicts
    
    * Change hardcoded data in currentTime variable
    
    Co-authored-by: Brandon <brandonzhang00@gmail.com>
    Co-authored-by: Newton Huynh <55329846+newton-huynh@users.noreply.github.com>
    Co-authored-by: Newton Huynh <nth8@rice.edu>

--- client/src/Pages/User/Menu/index.js
+++ client/src/Pages/User/Menu/index.js
@@ -152,9 +152,6 @@ function Menu () {
     style: 'currency',
     currency: 'USD'
 
-    // These options are needed to round to whole numbers if that's what you want.
-    // minimumFractionDigits: 0,
-    // maximumFractionDigits: 0,
   })
 
   formatter.format(2500)
--- client/src/Pages/User/Vendors/VendorCard.js
+++ client/src/Pages/User/Vendors/VendorCard.js
@@ -26,6 +26,7 @@ function VendorCard ({ vendor }) {
   // temporary fix:
   // const currentDay = 1
   const dayObj = hours[currentDay]
+  console.log(dayObj)
   const convertTimeToNum = time => {
     const [timeNum, halfOfDay] = time.split(' ')
     let [hours, minutes] = timeNum.split(':')
@@ -48,17 +49,32 @@ function VendorCard ({ vendor }) {
 
   const determineIfClosed = (current_date, dayObj) => {
     if (!dayObj) return
-    const currentTime = current_date.getHours() + current_date.getMinutes() / 60
-    const startTime = convertTimeToNum(dayObj.start[0])
-    const endTime = convertTimeToNum(dayObj.end[0])
-    return currentTime <= startTime || currentTime >= endTime
+    else if(dayObj.isClosed === true){
+      return true
+    }
+    else{
+      const currentTime = current_date.getHours() + current_date.getMinutes() / 60
+      const startTimes = dayObj.start.map((startTime) => {
+        return convertTimeToNum(startTime)
+      })
+      const endTimes = dayObj.end.map((endTime) => {
+        return convertTimeToNum(endTime)
+      })
+      let isClosedHours = true
+      for(let i = 0; i<startTimes.length; i++){
+        if(currentTime >= startTimes[i] && currentTime < endTimes[i]) {
+          isClosedHours = false
+        }
+      }
+      return isClosedHours
+    }
   }
 
   const closed = determineIfClosed(current_date, dayObj)
 
   return (
     <>
-      <div className='vendorContainer' onClick={() => handleClick()}>
+      <div className={closed ? 'vendorContainer vendorDisabled' : 'vendorContainer'} onClick={() => handleClick()}>
         <div className='vendorHeading'>
           <div className='vendorHeadingText'>
             <h3 className='vendorName'>{name}</h3>

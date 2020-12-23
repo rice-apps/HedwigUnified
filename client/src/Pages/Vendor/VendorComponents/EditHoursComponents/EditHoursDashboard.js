1e9260a857c199b134f0fa9ffc69fc232f7b2bef
Newton Huynh <nth8@rice.edu>
2020-12-22 19:23:12 -0600

    Create add hour modal without confirm button

--- client/src/Pages/Vendor/VendorComponents/EditHoursComponents/EditHoursDashboard.js
+++ client/src/Pages/Vendor/VendorComponents/EditHoursComponents/EditHoursDashboard.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { useState } from "react";
 import styled from "styled-components";
 import { IoMdClose, IoMdArrowDropdown } from "react-icons/io";
 import { CgMathPlus } from "react-icons/cg";
+import Modal from "react-modal";
 
 const EditHoursDashboardWrapper = styled.div`
   height: 90%;
@@ -62,22 +64,20 @@ const StatusDropdown = styled.select`
   border-radius: 10px;
   -webkit-appearance: none;
   position: relative;
-  padding-right:19px;
+  padding-right: 19px;
 `;
 
-
 function CreateStatusDropdown() {
-    return (
-    
-        <StatusColumn>
-          <StatusDropdown name="storeStatus" id="storeStatus">
-            <option value="OPEN">Open</option>
-            <option value="CLOSED">Closed</option>
-          </StatusDropdown>
-          <IoMdArrowDropdown style={{position:"absolute", right:10}}/>
-        </StatusColumn>
-    );
-  }
+  return (
+    <StatusColumn>
+      <StatusDropdown name="storeStatus" id="storeStatus">
+        <option value="OPEN">Open</option>
+        <option value="CLOSED">Closed</option>
+      </StatusDropdown>
+      <IoMdArrowDropdown style={{ position: "absolute", right: "1.1vw" }} />
+    </StatusColumn>
+  );
+}
 
 const HoursColumn = styled.div`
   grid-area: Hours;
@@ -137,13 +137,91 @@ const AddButton = styled.div`
   display: flex;
   align-items: center;
 `;
-function MakeAddHoursButton() {
+
+const AddHourModalWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 0.5fr 1.5fr 0.2fr 1.5fr;
+  grid-template-rows: 1.4fr 1fr;
+  height:80%;
+  width:100%;
+  position:absolute;
+  grid-template-areas:
+    "DOTW StartTime ToSpace EndTime"
+    "Confirm Confirm Confirm Confirm";
+  align-items: center;
+  justify-content: center;
+  font-size: 2.8vh;
+  padding:10px 15px;
+`;
+
+const DayModal = styled.div`
+  grid-area: DOTW;
+  text-align: right;
+`;
+
+const TimeModal = styled.input`
+  background-color: #f4f3f3;
+  border-radius: 10px;
+  padding: 6px;
+  position: relative;
+  display: block;
+  margin: 0 auto;
+  font-size:2.5vh;
+  border: none;
+  text-align: center;
+  
+`;
+
+function MakeTimeInput(props) {
+    let side = props.side
+  return (
+    <div>
+      <TimeModal type="time"></TimeModal>
+    </div>
+  );
+}
+
+function MakeAddHoursButton(props) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  function openAddHourModal () {
+      setModalIsOpen(true)
+  }
+  function closeAddHourModal () {
+      setModalIsOpen(false)
+  }
   return (
     <AddColumn>
-      <AddButton>
+      <AddButton onClick={openAddHourModal}>
         <CgMathPlus />
-        <div>Add Hours</div>
+        <div >Add Hours</div>
       </AddButton>
+      <Modal
+        isOpen={modalIsOpen}
+        style={{
+          content: {
+            backgroundColor: "white",
+            height: "25vh",
+            width: "44vw",
+            position: "absolute",
+            top: "36%",
+            left: "28%",
+            border: "3px solid #9D9D9D45",
+            borderRadius: "20px"
+          }
+        }}
+      >
+          <form>
+        <AddHourModalWrapper>
+          <DayModal>{props.weekday}</DayModal>
+          <MakeTimeInput id="addedStartTime"/>
+          <div tyle={{textAlign:"middle"}}> TO </div>
+          <MakeTimeInput id="addedEndTime"/>
+          
+        </AddHourModalWrapper>
+        <IoMdClose onClick={closeAddHourModal} style={{position:"absolute", top:"15px", right:"15px", fontSize:"3vh"}}/>
+        </form>
+      </Modal>
     </AddColumn>
   );
 }
@@ -163,7 +241,7 @@ function EditHoursDashboard() {
                 <HoursItem startTime="1:00 PM" endTime="6:00 PM"></HoursItem>
                 <HoursItem startTime="8:00 PM" endTime="11:00 PM"></HoursItem>
               </HoursColumn>
-              <MakeAddHoursButton />
+              <MakeAddHoursButton weekday={day} />
             </EditHoursRow>
           );
         })}

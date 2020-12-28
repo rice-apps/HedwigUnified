94f341e9fa3ac6b4302217d4f1d30d443c91ba1f
Victor Song <vms2@rice.edu>
2020-12-28 02:00:11 -0600

    Minor server context fixes

--- api/src/index.js
+++ api/src/index.js
@@ -16,15 +16,7 @@ const app = express().use(cors())
 
 const server = new ApolloServer({
   schema: Schema,
-  introspection: true,
-  context: ({ req, res }) => {
-    return {
-      keyMap: {
-        ZETW20E2NB4EG: process.env.SQUARE_ACCESS_TOKEN,
-        FMXAFFWJR95WC: process.env.SQUARE_ACCESS_TOKEN
-      }
-    }
-  }
+  introspection: true
 })
 
 server.applyMiddleware({ app })

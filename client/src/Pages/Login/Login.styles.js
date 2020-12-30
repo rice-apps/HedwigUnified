48c4c0c64db2d5f5c738f88c43562e497faed7ae
Yaning Hu <helenahu@users.noreply.github.com>
2020-12-30 21:34:52 +0800

    fixing colors and logos. Still need to change font family

---
+++ client/src/Pages/Login/Login.styles.js
@@ -0,0 +1,162 @@
+import styled from 'styled-components'
+
+const MainDiv = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  max-width: 100%;
+  font-family: 'Raleway', sans-serif;
+  background-color: white;
+`
+
+const ElemDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  align-items: center;
+  justify-items: center;
+  max-height: 70%;
+`
+
+const Logo = styled.img`
+  width: 10rem;
+`
+
+const HedwigLogo = styled.img`
+  max-height: 20%;
+  max-width: 20%;
+`
+const Title = styled.h1`
+  color: #f7735a;
+  font-size: 3.3rem;
+  margin: 0.1rem;
+`
+const LoginButton = styled.button`
+  border-radius: 25pt;
+  height: 2.2rem;
+  width: 8.5rem;
+  border: none;
+  font-size: 0.8rem;
+  font-weight: bold;
+  background-color: #f7735a;
+  color: white;
+  :hover {
+    text-decoration: underline;
+    box-shadow: 2px 8px 5px -5px #9d7a96;
+  }
+`
+
+const BackgroundCover = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: grid;
+  background-color: white;
+  max-width: 100%;
+  font-family: 'Raleway', sans-serif;
+
+  // for the internal components of this div
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  @media (max-aspect-ratio: 848/712) {
+    width: 100vw;
+    height: 89vh;
+    top: 11vh;
+    left: 25vw;
+    // background-color: #ffffff;
+  }
+`
+
+const ButtonPane = styled.div`
+  background-color: #f49f86;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+`
+
+const VendorButton = styled.button`
+  grid-row: 4/5;
+  border-radius: 25pt;
+  height: 40vh;
+  width: 40%;
+  border: none;
+  font-size: 14pt;
+  font-weight: bold;
+  border: 3px solid blue;
+  background-color: white;
+  cursor: pointer;
+  color: #f49f86;
+  :hover {
+    text-decoration: underline;
+    box-shadow: 2px 8px 5px -5px #9d7a96;
+  }
+`
+
+const ClientButton = styled.button`
+  grid-row: 4/5;
+  border-radius: 25pt;
+  height: 40vh;
+  width: 40%;
+  border: none;
+  font-size: 14pt;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+
+  font-weight: bold;
+  color: #f49f86;
+  border: 3px solid orange;
+  background-color: white;
+  cursor: pointer;
+  :hover {
+    text-decoration: underline;
+    box-shadow: 2px 8px 5px -5px #9d7a96;
+  }
+`
+
+const ExitButton = styled.button`
+  position: relative;
+  background-color: white;
+  bottom: 5pt;
+  border: 1px solid red;
+  font-weight: bold;
+
+  text-align: center;
+  cursor: pointer;
+  :hover {
+    background-color: #ffe6e6;
+    text-decoration: underline;
+    box-shadow: 2px 8px 5px -5px #9d7a96;
+  }
+`
+
+const LoginQuestion = styled.p`
+  position: relative;
+  font-weight: bold;
+  color: white;
+  font-size: 25pt;
+  text-align: center;
+  padding: 1em;
+`
+
+export {
+  MainDiv,
+  ElemDiv,
+  Logo,
+  Title,
+  LoginButton,
+  BackgroundCover,
+  ButtonPane,
+  VendorButton,
+  ClientButton,
+  ExitButton,
+  LoginQuestion,
+  HedwigLogo
+}
---
+++ client/src/Pages/Login/index.js
@@ -0,0 +1,79 @@
+import { useState } from 'react'
+import { useQuery, gql } from '@apollo/client'
+
+import logo from './HedwigLogoFinal_02.svg'
+import { SERVICE_URL } from '../../config'
+import { MainDiv, ElemDiv, Logo, Title, LoginButton } from './Login.styles'
+// import './Transitions.css';
+import { userProfile } from '../../apollo'
+import { useNavigate } from 'react-router-dom'
+
+// This import loads the firebase namespace along with all its type information.
+import firebase from 'firebase/app'
+
+// These imports load individual services into the firebase namespace.
+import 'firebase/auth'
+
+const casLoginURL = 'https://idp.rice.edu/idp/profile/cas/login'
+
+const sStorage = window.localStorage
+
+function Login () {
+  const navigate = useNavigate()
+
+  // const provider = new firebase.auth.SAMLAuthProvider("saml.jumpcloud-demo");
+  const provider = new firebase.auth.SAMLAuthProvider('saml.rice-shibboleth')
+  // Fetch service from cache since it depends on where this app is deployed
+  // const { data } = useQuery(GET_SERVICE_LOCAL);
+
+  const casLoginURL = 'https://idp.rice.edu/idp/profile/cas/login'
+
+  // Handles click of login button
+  const login = () => {
+    // Redirects user to the CAS login page
+    const redirectURL = casLoginURL + '?service=' + SERVICE_URL
+    window.open(redirectURL, '_self')
+  }
+
+  const signInSAML = () => {
+    firebase
+      .auth()
+      .signInWithRedirect(provider)
+      .then(result => {
+        console.log(result)
+        console.log(result.user)
+        console.log(result.additionalUserInfo)
+      })
+      .catch(error => console.log(error))
+  }
+
+  firebase
+    .auth()
+    .getRedirectResult()
+    .then(result => {
+      if (result.user) {
+        const profile = result.additionalUserInfo.profile
+        // redirect to auth page carrying state from IDP
+        sStorage.setItem('last name', profile['urn:oid:2.5.4.4'])
+        sStorage.setItem('first name', profile['urn:oid:2.5.4.42'])
+        sStorage.setItem('email', profile['urn:oid:0.9.2342.19200300.100.1.3'])
+        sStorage.setItem('id', profile['urn:oid:1.3.6.1.4.1.134.1.1.1.1.19'])
+        login()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+    })
+
+  return (
+    <MainDiv>
+      <ElemDiv>
+        <Logo src={logo} />
+        <Title>hedwig</Title>
+        <LoginButton onClick={signInSAML}>Login with NetID</LoginButton>
+      </ElemDiv>
+    </MainDiv>
+  )
+}
+
+export default Login

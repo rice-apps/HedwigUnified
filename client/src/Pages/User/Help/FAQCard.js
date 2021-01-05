32822facb7ad38ce4baa6f19579a995b18fe868c
Henry Qin <hqin2400@gmail.com>
2021-01-05 01:40:13 -0800

    formatted each answer

---
+++ client/src/Pages/User/Help/FAQCard.js
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+
+import {
+  IoIosArrowUp,
+  IoIosArrowDown
+} from 'react-icons/io'
+
+import {
+  QuestionSection,
+  QuestionText,
+  ExpandSection,
+  AnswerSection
+} from './HelpPage.styles'
+
+function FAQCard (props) {
+  const [showAnswer, setShowAnswer] = useState(false)
+
+  return (
+    <>
+      <QuestionSection
+        onClick={() => {
+          setShowAnswer(!showAnswer)
+        }}
+        style={showAnswer ? { 
+          color: '#F3725B',
+          borderBottom: 'none'
+        } : { 
+          color: '#333333',
+          borderBottom: '#5A59534D 1px solid'
+        }}
+      > 
+        <QuestionText> {props.question} </QuestionText>
+        <ExpandSection>
+          {showAnswer ? (
+            <IoIosArrowUp />
+          ) : (
+            <IoIosArrowDown />
+          )}
+          
+        </ExpandSection>
+      </QuestionSection>
+      {showAnswer ? (
+        <AnswerSection>{props.answer}</AnswerSection>
+      ) : null}
+    </>
+  )
+}
+
+export default FAQCard
---
+++ client/src/Pages/User/Help/HelpPage.styles.js
@@ -0,0 +1,81 @@
+import styled, { css } from 'styled-components/macro'
+
+const HelpHeader = styled.section`
+  display: grid;
+  grid-template-columns: 16vw 1fr 16vw;
+  justify-items: center;
+  align-items: center;
+
+  height: 10vh;
+  background-color: white;
+
+  font-family: 'Avenir-black';
+  font-size: 6vw;
+`
+
+const BackArrowSection = styled.section`
+  display: grid;
+  font-size: 8vw;
+`
+
+const FAQSection = styled.section`
+  padding: 0vh 5vw;
+`
+
+const FAQHeader = styled.section`
+  padding: 0vh 0vw 1vh 0vw;
+  font-family: 'Avenir-black';
+  font-size: 6vw;
+`
+
+const SubHeader = styled.section`
+  padding: 2vh 0vw 1vh 0vw;
+
+  font-family: 'Avenir-black';
+  font-size: 5vw;
+`
+
+const QuestionSection = styled.section`
+  display: grid;
+  grid-template-columns: 1fr 5vw;
+  align-items: center;
+
+  height: auto;
+
+  padding: 1vh 0vw;
+`
+
+const QuestionText = styled.section`
+  font-family: 'Avenir';
+  font-size: 4.5vw;
+  margin-right: 4vw;
+`
+
+const ExpandSection = styled.section`
+  font-size: 6vw;
+  margin-bottom: -0.5vh;
+`
+
+const AnswerSection = styled.section`
+  height: auto;
+  white-space: pre-wrap;
+
+  font-family: 'Avenir';
+  color: #5A5953;
+  font-size: 4vw;
+
+  padding: 0vh 0vw 1vh 0vw;
+  border-bottom: #5A59534D 1px solid;
+`
+
+export {
+  HelpHeader,
+  BackArrowSection,
+  FAQSection,
+  FAQHeader,
+  SubHeader,
+  QuestionSection,
+  QuestionText,
+  ExpandSection,
+  AnswerSection
+}

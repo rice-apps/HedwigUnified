6b7346b8bedf704493058c619ffcd812dfd69867
Ananya Vaidya <akv4@rice.edu>
2021-01-06 17:55:36 -0800

    added updateBasicInfo mutation

--- client/src/Pages/Vendor/VendorComponents/SetBasicInfoComponents/BasicInfoDashboard.js
+++ client/src/Pages/Vendor/VendorComponents/SetBasicInfoComponents/BasicInfoDashboard.js
@@ -1,20 +1,85 @@
-import styled, {css} from 'styled-components'
+import styled, { css } from "styled-components";
+import { gql, useQuery, useMutation, useApolloClient } from "@apollo/client";
+
+const GET_VENDOR_INFO = gql`
+  query GET_AVAILABILITY($name: String!) {
+    getVendor(filter: { name: "Cohen House" }) {
+      name
+      website
+      email
+      facebook
+      phone
+      cutoffTime
+      pickupInstruction
+    }
+  }
+`;
+
+const UPDATE_VENDOR = gql`
+  mutation UPDATE_VENDOR(
+    $name: String!
+    $website: String!
+    $email: String!
+    $facebook: String!
+    $phone: String!
+    $cutoffTime: Float!
+    $pickupInstruction: String!
+  ) {
+    updateVendor(
+      record: {
+        website: $website
+        email: $email
+        facebook: $facebook
+        phone: $phone
+        cutoffTime: $cutoffTime
+        pickupInstruction: $pickupInstruction
+      }
+      filter: { name: $name }
+    ) {
+      record {
+        website
+        email
+        facebook
+        phone
+        cutoffTime
+        pickupInstruction
+      }
+    }
+  }
+`;
 
 const Div = styled.div`
-${props => props.wrapper && css`
-height:95%;
-width:85%;
-background-color:orange;
-display: grid;
-`}
-`
+  ${(props) =>
+    props.wrapper &&
+    css`
+      height: 95%;
+      width: 85%;
+      background-color: orange;
+      display: grid;
+    `}
+`;
 
 function BasicInfoDashboard() {
-    return(
-    <Div wrapper>
-Set Basic Info
-    </Div>)
+  const [updateBasicInfo, { data, loading, error }] = useMutation(
+    UPDATE_VENDOR
+  );
+
+  console.log("data ", data);
+
+  updateBasicInfo({
+    variables: {
+      name: "Cohen House",
+      website: "https://facultyclub.rice.edu/contact-us",
+      email: "club@rice.edu",
+      facebook: "test",
+      phone: "713-348-4000",
+      cutoffTime: 3.5,
+      pickupInstruction: "test",
+    },
+  });
+
+  console.log(data);
+  return <Div wrapper>Set Basic Info</Div>;
 }
 
-
-export default BasicInfoDashboard
+export default BasicInfoDashboard;

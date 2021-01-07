348ffc6b13e3f18ee74a0772bae8208257eec6ae
Newton Huynh <nth8@rice.edu>
2021-01-07 13:44:25 -0600

    Restyle sidebar

--- client/src/Pages/Vendor/VendorComponents/SetBasicInfoComponents/BasicInfoDashboard.js
+++ client/src/Pages/Vendor/VendorComponents/SetBasicInfoComponents/BasicInfoDashboard.js
@@ -1,5 +1,5 @@
-import styled, { css } from "styled-components";
-import { gql, useQuery, useMutation, useApolloClient } from "@apollo/client";
+import styled, { css } from 'styled-components'
+import { gql, useQuery, useMutation, useApolloClient } from '@apollo/client'
 
 const GET_VENDOR_INFO = gql`
   query GET_AVAILABILITY($name: String!) {
@@ -13,7 +13,7 @@ const GET_VENDOR_INFO = gql`
       pickupInstruction
     }
   }
-`;
+`
 
 const UPDATE_VENDOR = gql`
   mutation UPDATE_VENDOR(
@@ -46,40 +46,57 @@ const UPDATE_VENDOR = gql`
       }
     }
   }
-`;
+`
 
 const Div = styled.div`
-  ${(props) =>
+  ${props =>
     props.wrapper &&
     css`
       height: 95%;
       width: 85%;
       background-color: orange;
       display: grid;
+      grid-template-columns: 1fr 1fr;
+      grid-template-rows: 2fr 1fr 1fr 1fr 1.5fr 2fr 1.5fr;
+      grid-template-areas:
+        'Image Image'
+        'Name Website'
+        'Email Facebook'
+        'Phone Blank'
+        'Cutoff Cutoff'
+        'Pickup Pickup'
+        'Button Button';
     `}
-`;
+  ${props => props.wrapper && css``}
+`
 
-function BasicInfoDashboard() {
-  const [updateBasicInfo, { data, loading, error }] = useMutation(
-    UPDATE_VENDOR
-  );
+const Img = styled.img`
+  ${props => props.logo && css`
+  height:15vh;
+  width:15vh;
+  border-radius: 50%
+  `}
+`
 
-  console.log("data ", data);
+function BasicInfoDashboard () {
+  const [updateBasicInfo, { data, loading, error }] = useMutation(UPDATE_VENDOR)
+
+  console.log('data ', data)
 
   updateBasicInfo({
     variables: {
-      name: "Cohen House",
-      website: "https://facultyclub.rice.edu/contact-us",
-      email: "club@rice.edu",
-      facebook: "test",
-      phone: "713-348-4000",
-      cutoffTime: 3.5,
-      pickupInstruction: "test",
-    },
-  });
+      name: 'Cohen House',
+      website: 'https://facultyclub.rice.edu/contact-us',
+      email: 'club@rice.edu',
+      facebook: 'facebook.com/rice',
+      phone: '713-348-4000',
+      cutoffTime: 15,
+      pickupInstruction: 'test'
+    }
+  })
 
-  console.log(data);
-  return <Div wrapper>Set Basic Info</Div>;
+  console.log(data)
+  return <Div wrapper>Set Basic Info</Div>
 }
 
-export default BasicInfoDashboard;
+export default BasicInfoDashboard

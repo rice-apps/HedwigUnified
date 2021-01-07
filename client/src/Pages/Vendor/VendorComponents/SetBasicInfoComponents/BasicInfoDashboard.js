63e2b9d230204a69ae3b149aeee45b9178e48505
Newton Huynh <nth8@rice.edu>
2021-01-07 15:28:46 -0600

    Add input and detail wrapper

--- client/src/Pages/Vendor/VendorComponents/SetBasicInfoComponents/BasicInfoDashboard.js
+++ client/src/Pages/Vendor/VendorComponents/SetBasicInfoComponents/BasicInfoDashboard.js
@@ -3,8 +3,9 @@ import { gql, useQuery, useMutation, useApolloClient } from '@apollo/client'
 
 const GET_VENDOR_INFO = gql`
   query GET_AVAILABILITY($name: String!) {
-    getVendor(filter: { name: "Cohen House" }) {
+    getVendor(filter: { name: $name }) {
       name
+      logoUrl
       website
       email
       facebook
@@ -49,54 +50,116 @@ const UPDATE_VENDOR = gql`
 `
 
 const Div = styled.div`
+font-size:2.8vh;
   ${props =>
     props.wrapper &&
     css`
-      height: 95%;
-      width: 85%;
-      background-color: orange;
+      height: 90%;
+      width: 90%;
+      background-color: white;
+      border-radius: 15px;
       display: grid;
       grid-template-columns: 1fr 1fr;
-      grid-template-rows: 2fr 1fr 1fr 1fr 1.5fr 2fr 1.5fr;
+      grid-template-rows: 2.8fr 0.8fr 0.8fr 0.8fr 1.5fr 2fr 1.2fr;
       grid-template-areas:
-        'Image Image'
+        'ImageSpace ImageSpace'
         'Name Website'
         'Email Facebook'
         'Phone Blank'
         'Cutoff Cutoff'
         'Pickup Pickup'
         'Button Button';
+      justify-items: center;
+      align-items: center;
     `}
-  ${props => props.wrapper && css``}
+  ${props =>
+    props.detail &&
+    css`
+      display: grid;
+      grid-area: ${props => props.gridArea};
+      grid-template-rows: 1fr;
+      grid-template-columns: 0.8fr 2fr;
+      height:100%;
+      width:100%;
+      align-items: center;
+    `}
+ ${props =>
+   props.detailTitle &&
+   css`
+     font-size: 2.35vh;
+     font-weight: 500;
+     text-align:right
+   `}
 `
 
 const Img = styled.img`
-  ${props => props.logo && css`
-  height:15vh;
-  width:15vh;
-  border-radius: 50%
-  `}
+  ${props =>
+    props.logo &&
+    css`
+      grid-area: ImageSpace;
+      height: 18vh;
+      width: 18vh;
+      border-radius: 50%;
+      margin-top: 10px;
+    `}
 `
+
+const Input = styled.input`
+ height: 4.5vh;
+ border: none;
+ border-radius:20px;
+background-color:#f1f1f1;
+width: 80%;
+margin-left:1rem;
+font-size:2.3vh;
+padding-left: 15px;
+`
+
+function BasicInfoDetail (props) {
+  return (
+    <Div detail gridArea={props.gridArea}>
+      <Div detailTitle>{props.gridArea}:</Div>
+    <Input type="text"></Input>
+    </Div>
+  )
+}
 
 function BasicInfoDashboard () {
   const [updateBasicInfo, { data, loading, error }] = useMutation(UPDATE_VENDOR)
-
-  console.log('data ', data)
-
-  updateBasicInfo({
-    variables: {
-      name: 'Cohen House',
-      website: 'https://facultyclub.rice.edu/contact-us',
-      email: 'club@rice.edu',
-      facebook: 'facebook.com/rice',
-      phone: '713-348-4000',
-      cutoffTime: 15,
-      pickupInstruction: 'test'
-    }
+  const {
+    data: vendorData,
+    loading: vendorLoading,
+    error: vendorError
+  } = useQuery(GET_VENDOR_INFO, {
+    variables: { name: 'Cohen House' }
   })
+  if (vendorError) {return <p>Error</p>}
+  if (vendorLoading) {return <p>Waiting...</p>}
+  console.log('vendorData ', vendorData.getVendor.logoUrl)
 
-  console.log(data)
-  return <Div wrapper>Set Basic Info</Div>
+  //   updateBasicInfo({
+  //     variables: {
+  //       name: 'Cohen House',
+  //       website: 'https://facultyclub.rice.edu/contact-us',
+  //       email: 'club@rice.edu',
+  //       facebook: 'facebook.com/rice',
+  //       phone: '713-348-4000',
+  //       cutoffTime: 15,
+  //       pickupInstruction: 'test'
+  //     }
+  //   })
+
+  //   console.log(data)
+  return (
+    <Div wrapper>
+      <Img logo src={vendorData.getVendor.logoUrl}/>
+      <BasicInfoDetail gridArea='Name' />
+      <BasicInfoDetail gridArea='Website'/>
+      <BasicInfoDetail gridArea='Email'/>
+      <BasicInfoDetail gridArea='Facebook'/>
+      <BasicInfoDetail gridArea='Phone'/>
+    </Div>
+  )
 }
 
 export default BasicInfoDashboard

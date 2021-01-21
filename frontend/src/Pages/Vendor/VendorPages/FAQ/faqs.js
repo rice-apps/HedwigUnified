9bb6d8e2e800690794f835570bfc1a308f723d6a
spoken-angus <angusjelinek@gmail.com>
2021-01-21 00:55:13 -0500

    faq copy fix

--- frontend/src/Pages/Vendor/VendorPages/FAQ/faqs.js
+++ frontend/src/Pages/Vendor/VendorPages/FAQ/faqs.js
@@ -1,97 +1,96 @@
 export const catalog_faqs = [
   {
-    question: 'How do I add or delete an item on the menu?',
+    question: "How do I add or delete an item on the menu?",
     answer: [
-      'The data for the menu is in Square. To add or delete an item on the menu, please login to the Square account and make the changes there.'
-    ]
+      "The data for the menu is in Square. To add or delete an item on the menu, please login to the Square account and make the changes there.",
+    ],
   },
   {
-    question: 'How do I change an item’s modifiers?',
+    question: "How do I change an item’s modifiers?",
     answer: [
-      'Modifier information is stored in Square. To edit, add, or remove modifiers for an item, please login to the Square account and make the changes there.'
-    ]
+      "Modifier information is stored in Square. To edit, add, or remove modifiers for an item, please login to the Square account and make the changes there.",
+    ],
   },
   {
-    question: 'How do I change an item’s variants?',
+    question: "How do I change an item’s variants?",
     answer: [
-      'Variant information is stored in Square. To edit, add, or remove variants for an item, please login to the Square account and make the changes there.'
-    ]
-  }
-]
+      "Variant information is stored in Square. To edit, add, or remove variants for an item, please login to the Square account and make the changes there.",
+    ],
+  },
+];
 
 export const order_faqs = [
   {
-    question: 'How do I accept an order paid in Tetra?',
+    question: "How do I accept an order paid in Tetra?",
     answer: [
-      'Select “View Payment Details” to view the student ID.',
-      'Enter the student ID and amount to charge into the Tetra reader.',
-      'If the transaction is successful:',
+      "Select “View Payment Details” to view the student ID.",
+      "Enter the student ID and amount to charge into the Tetra reader.",
+      "If the transaction is successful:",
       [
-        'Click “Accept Order” on Hedwig.',
-        'Find the newly accepted order and enter the order information into Revention to generate a ticket.'
+        "Click “Accept Order” on Hedwig.",
+        "Find the newly accepted order and enter the order information into Revention to generate a ticket.",
       ],
-      'If the transaction is unsuccessful: ',
+      "If the transaction is unsuccessful: ",
       [
-        'Click “Cancel Order” on Hedwig.',
-        '(If you wish to contact the buyer explaining why the order was cancelled, you can view buyer information by clicking the dropdown near their name at the top of an order card.)'
-      ]
-    ]
-  },
-  {
-    question: 'How do I accept an order paid in a Credit Card?',
-    answer: [
-      'Credit card transactions are processed by Shopify, which means the buyer will need to enter in their credit card information in a separate site for payment processing. This means the buyer may need a few more minutes after submitting the order to enter in their credit card information.',
-      'Select “View Payment Details” to view whether the transaction had been completed.',
-      'If the transaction is complete, you will see a “verified” status. Then you can accept or cancel the order.',
-      'If the translation is incomplete, you will see the option to “Return to Home” or Cancel the order. We suggest waiting for at least 10 minutes before cancelling the order.'
-    ]
-  },
-  {
-    question:
-      '[Cohen House Only] How do I accept an order paid in Cohen House Club Card?',
-    answer: [
-      'Select “View Payment Details” to view the Cohen House Club Card ID. ',
-      'Enter the Club Card ID and amount to charge into the Revention POS System.',
-      'If the transaction is successful:',
-      [
-        'Click “Accept Order” on Hedwig.',
-        'Find the newly accepted order and enter the order information into Revention to generate a ticket.'
+        "Click “Cancel Order” on Hedwig.",
+        "(If you wish to contact the buyer explaining why the order was cancelled, you can view buyer information by clicking the dropdown near their name at the top of an order card.)",
       ],
-      'If the transaction is unsuccessful:',
+    ],
+  },
+  {
+    question: "How do I accept an order paid in a Credit Card?",
+    answer: [
+      "Credit card transactions are processed by Shopify, which means the buyer will need to enter in their credit card information in a separate site for payment processing. This means the buyer may need a few more minutes after submitting the order to enter in their credit card information.",
+      "Select “View Payment Details” to view whether the transaction had been completed.",
+      "If the transaction is complete, you will see a “verified” status. Then you can accept or cancel the order.",
+      "If the translation is incomplete, you will see the option to “Return to Home” or Cancel the order. We suggest waiting for at least 10 minutes before cancelling the order.",
+    ],
+  },
+  {
+    question: "How do I accept an order paid in Cohen House Club Card?",
+    answer: [
+      "Select “View Payment Details” to view the Cohen House Club Card ID. ",
+      "Enter the Club Card ID and amount to charge into the Revention POS System.",
+      "If the transaction is successful:",
       [
-        'Click “Cancel Order” on Hedwig.',
-        '(If you wish to contact the buyer explaining why the order was cancelled, you can view buyer information by clicking the dropdown near their name at the top of an order card.)'
-      ]
-    ]
+        "Click “Accept Order” on Hedwig.",
+        "Find the newly accepted order and enter the order information into Revention to generate a ticket.",
+      ],
+      "If the transaction is unsuccessful:",
+      [
+        "Click “Cancel Order” on Hedwig.",
+        "(If you wish to contact the buyer explaining why the order was cancelled, you can view buyer information by clicking the dropdown near their name at the top of an order card.)",
+      ],
+    ],
   },
   {
-    question: 'How do I cancel an order?',
+    question: "How do I cancel an order?",
     answer: [
-      'Orders can be cancelled at any time. To cancel an order, select the “Cancel Order” option in the “New” orders column.'
-    ]
+      "Orders can be cancelled at any time. To cancel an order, select the “Cancel Order” option in the “New” orders column.",
+    ],
   },
   {
-    question: 'What happens when I accept/ready/pickup an order?',
+    question: "What happens when I accept/ready/pickup an order?",
     answer: [
-      'Whenever you change the status of an order, the buyer will receive a sms message notifying them of the change in status.'
-    ]
-  }
-]
+      "Whenever you change the status of an order, the buyer will receive a sms message notifying them of the change in status.",
+    ],
+  },
+];
 
 export const store_faqs = [
   {
-    question: 'How can I close the store early?',
+    question: "How can I close the store early?",
     answer: [
-      'The data for the menu is in Square. To add or delete an item on the menu, please login to the Square account and make the changes there.'
-    ]
+      "The data for the menu is in Square. To add or delete an item on the menu, please login to the Square account and make the changes there.",
+    ],
   },
   {
-    question: 'How do I set or delete store hours?',
+    question: "How do I set or delete store hours?",
     answer: [
-      'To set or delete store hours, navigate to the “Set Store Hours” page under the “Store Status” tab on the left hand side navigation pane.',
-      'To add hours, select the “Add Store Hours” button. You can only add non-overlapping intervals.',
-      'To delete hours, select the “x” in the top right corner of the time interval you would like to delete.',
-      'To indicate the store is closed for a certain day, select the “Close” option in the first dropdown next to the day of the week.'
-    ]
-  }
-]
+      "To set or delete store hours, navigate to the “Set Store Hours” page under the “Store Status” tab on the left hand side navigation pane.",
+      "To add hours, select the “Add Store Hours” button. You can only add non-overlapping intervals.",
+      "To delete hours, select the “x” in the top right corner of the time interval you would like to delete.",
+      "To indicate the store is closed for a certain day, select the “Close” option in the first dropdown next to the day of the week.",
+    ],
+  },
+];

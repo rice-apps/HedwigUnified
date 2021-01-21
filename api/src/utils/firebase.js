94da682eb784983798bd7e4bfb61cd9a58464534
Lorraine-Lyu <yl191@rice.edu>
2021-01-21 00:50:46 -0600

    deleted api and client files(accidently included in previous commits

--- api/src/utils/firebase.js
+++
@@ -1,8 +0,0 @@
-import * as admin from 'firebase-admin'
-
-const firebaseAdmin = admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
-  databaseURL: 'https://hedwig-279117.firebaseio.com'
-})
-
-export default firebaseAdmin

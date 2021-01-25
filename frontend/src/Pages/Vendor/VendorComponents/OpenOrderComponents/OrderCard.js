33cf970c29035f9839b2941143aae31afe0c22cf
Newton Huynh <nth8@rice.edu>
2021-01-24 21:52:53 -0600

    Restyle order card

--- frontend/src/Pages/Vendor/VendorComponents/OpenOrderComponents/OrderCard.js
+++ frontend/src/Pages/Vendor/VendorComponents/OpenOrderComponents/OrderCard.js
@@ -13,52 +13,35 @@ import gql from 'graphql-tag.macro'
 import { GrRestaurant } from 'react-icons/gr'
 import ORDER_TRACKER from '../../../../graphql/OrderTracker'
 import VERIFY_PAYMENT from '../../../../graphql/VerifyPayment'
+import {
+  OrderCardWrapper,
+  OrderTitleSpaceWrapper,
+  OrderTimeSpaceWrapper,
+  ExactTimeSpaceWrapper,
+  TimeLeftSpaceWrapper,
+  OrderDetailsSpaceWrapper,
+  OrderDetailsItemWrapper,
+  ItemDescriptionWrapper,
+PaymentSpaceWrapper,
+CostSpaceWrapper,
+ButtonsSpaceWrapper,
+AcceptButton,
+CancelButton,
+ReadyButton,
+PickedUpButton,
+ModalWrapper,
+ModalHeaderWrapper,
+ModalPaymentWrapper,
+ModalParagraphWrapper,
+ModalOrderDetailsWrapper,
+ModalOrderDetailRow,
+ModalButtonsWrapper
+} from './OrderCard.styles'
 
 const formatter = new Intl.NumberFormat('en-US', {
   style: 'currency',
   currency: 'USD'
 })
-
-const OrderCardWrapper = styled.div`
-  background-color: white;
-  border-radius: 20px;
-  border-width: 2px;
-  border-color: #cacaca;
-  border-style: solid;
-  justify-self: center;
-
-  display: grid;
-  width: 26vw;
-  height: max-content;
-  margin: 10px;
-  margin-right: 14px;
-  overflow: visible;
-  grid-template-columns: 1fr;
-  grid-template-rows: max-content max-content max-content 92px;
-  grid-template-areas:
-    'OrderTitleSpace'
-    'OrderTimeSpace'
-    'OrderDetailsSpace'
-    'PaymentSpace';
-`
-
-const OrderTitleSpaceWrapper = styled.div`
-  background-color: white;
-  font-weight: bolder;
-  grid-area: OrderTitleSpace;
-  display: grid;
-  grid-template-columns: 1fr 2fr 1fr;
-  grid-template-rows: 1fr;
-  font-size: 22px;
-  line-height: 23.5px;
-  justify-content: center;
-  justify-items: center;
-  padding-top: 8px;
-  align-items: center;
-  overflow: hidden;
-  border-top-right-radius: 20px;
-  border-top-left-radius: 20px;
-`
 
 function MakeOrderTitle (props) {
   return (
@@ -69,33 +52,6 @@ function MakeOrderTitle (props) {
     </OrderTitleSpaceWrapper>
   )
 }
-
-const OrderTimeSpaceWrapper = styled.div`
-  background-color: white;
-  grid-area: OrderTimeSpace;
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-template-rows: 1fr;
-  font-size: 14px;
-  grid-template-areas: 'ExactTimeSpace TimeLeftSpace';
-  font-weight: 500;
-`
-
-const ExactTimeSpaceWrapper = styled.div`
-  grid-area: ExactTimeSpace;
-  display: flex;
-  flex-direction: column;
-  text-align: left;
-  margin-top: 7px;
-  margin-left: 10px;
-  line-height: 18px;
-`
-const TimeLeftSpaceWrapper = styled.div`
-  grid-area: TimeLeftSpace;
-  text-align: right;
-  color: #2d2d2d;
-  font-size: 13px;
-`
 
 function MakeOrderTime (props) {
   return (
@@ -122,41 +78,21 @@ function MakeOrderTime (props) {
   )
 }
 
-const OrderDetailsSpaceWrapper = styled.div`
-  background-color: #fafafa;
-  grid-area: OrderDetailsSpace;
-  display: flex;
-  flex-direction: column;
-`
-const OrderDetailsItemWrapper = styled.div`
-  background-color: #fafafa;
-  margin: 3px 0px;
-  display: grid;
-  grid-template-columns: 1.1fr 10fr 3fr;
-  grid-template-rows: 1fr;
-  font-size: 14px;
-`
-const ItemDescriptionWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  margin-left: 6px;
-`
 
 function MakeOrderDetails (props) {
   return (
     <OrderDetailsItemWrapper>
       <div style={{ fontWeight: 'bold' }}>{props.quantity}</div>
       <ItemDescriptionWrapper>
-        <div style={{ textTransform: 'uppercase', fontWeight: 'bold' }}>
+        <div style={{ textTransform: 'uppercase', fontWeight: 'bold', textAlign: 'left' }}>
           {props.itemName}
         </div>
-        <div>
-          <BiFoodMenu /> {props.variant}
+        <div style={{ display:'grid', gridTemplateColumns:'2.5vh 1fr', alignItmes:'center', textAlign:'left'}}>
+          <BiFoodMenu style={{alignSelf:'flex-start', marginTop:'0.3vh'}} /> {props.variant}
         </div>
         {props.modifiers && (
-          <div>
-            <IoIosAddCircleOutline /> {props.modifiers}
+          <div style={{ display:'grid', gridTemplateColumns:'2.5vh 1fr',alignItems:'center', textAlign:'left'}}>
+            <IoIosAddCircleOutline style={{alignSelf:'flex-start', marginTop:'0.3vh'}} /> {props.modifiers}
           </div>
         )}
       </ItemDescriptionWrapper>
@@ -165,87 +101,8 @@ function MakeOrderDetails (props) {
   )
 }
 
-const PaymentSpaceWrapper = styled.div`
-  background-color: white;
-  border-top: 1px solid grey;
-  width: 90%;
-  justify-self: center;
-  grid-area: PaymentSpace;
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-template-rows: 1fr 1.2fr;
-  font-size: 14px;
-`
 
-const CostSpaceWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: flex-end;
-  text-align: right;
-  margin-right: 5px;
-  margin-bottom: 7px;
-  margin-top: 3px;
-`
-const ButtonsSpaceWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-`
-const ButtonWrapper = styled.button`
-  border-radius: 20px;
-  cursor: pointer;
-  border: 0px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 5px 20px;
-  margin: 0px 15px;
-  margin-bottom: 15px;
-`
 
-const AcceptButton = styled(ButtonWrapper)`
-  background-color: #f9ddd7;
-`
-
-const CancelButton = styled(ButtonWrapper)`
-  background-color: #dedede;
-`
-
-const ReadyButton = styled(ButtonWrapper)`
-  background-color: #fadfbe;
-`
-const PickedUpButton = styled(ButtonWrapper)`
-  background-color: #deeee7;
-`
-
-const ModalWrapper = styled.div`
-  display: grid;
-  font-family: 'Metropolis';
-  grid-template-columns: 1fr;
-  grid-template-rows: 2fr 4fr 5fr 3fr;
-  grid-template-areas:
-    'ModalHeader'
-    'ModalParagraph'
-    'ModalOrderDetails'
-    'ModalButtons';
-  height: 100%;
-  width: 100%;
-  align-items: center;
-  color: black;
-`
-
-const ModalHeaderWrapper = styled.div`
-  grid-area: ModalHeader;
-  height: 100%;
-  width: 100%;
-  font-size: 30px;
-  align-items: center;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-`
 
 function MakeModalHeader (props) {
   const paymentType = props.paymentType
@@ -259,22 +116,6 @@ function MakeModalHeader (props) {
   )
 }
 
-const ModalParagraphWrapper = styled.div`
-  /* background-color: blue; */
-  height: 100%;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  text-align: center;
-  font-size: 16px;
-  grid-area: ModalParagraph;
-  border-bottom: 1px solid grey;
-`
-
-const ModalPaymentWrapper = styled.div`
-  color: #3d3d3d;
-  font-weight: bold;
-`
 
 function isEmpty (obj) {
   for (var prop in obj) {
@@ -347,24 +188,7 @@ function MakeModalParagraph (props) {
   }
 }
 
-const ModalOrderDetailsWrapper = styled.div`
-  grid-area: ModalOrderDetails;
-  height: 100%;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  border-bottom: 1px solid grey;
-`
 
-const ModalOrderDetailRow = styled.div`
-  width: 35%;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  margin: 1.2px 0px;
-`
 function MakeModalOrderDetails (props) {
   const paymentType = props.paymentType
   return (
@@ -393,14 +217,7 @@ function MakeModalOrderDetails (props) {
   )
 }
 
-const ModalButtonsWrapper = styled.div`
-  grid-area: ModalButtons;
-  margin-top: 4vh;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-`
+
 
 function MakePaymentSpace (props) {
   const [acceptModalIsOpen, setAcceptModalIsOpen] = useState(false)

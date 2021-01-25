b5b9f9d4c9c21e97451509872811cd640ef1c411
Victor Song <vms2@rice.edu>
2021-01-24 20:44:45 -0600

    Removed sensitive info and added refresh token stuff

--- backend/src/utils/square.js
+++ backend/src/utils/square.js
@@ -1,28 +1,30 @@
 import { Client, Environment } from 'square'
 import { Vendor } from '../models/index.js'
 
-
 // const squareClient = new Client({
 //   environment: Environment.Sandbox,
 //   accessToken: SQUARE_ACCESS_TOKEN
 // })
 
-const squareClients = new Promise((resolve,reject) => {
-    Vendor.find()
+const squareClients = new Promise((resolve, reject) => {
+  Vendor.find()
     .exec()
     .then(res => {
-
-        const squareClientsMap = new Map()
-        res.forEach(vendor => {
-          squareClientsMap.set(vendor.name, new Client({environment: Environment.Sandbox,accessToken: vendor.squareInfo.accessToken}))
-        })
-        return resolve(squareClientsMap)
-
-  })
-    .catch((error) => {
-        reject(error)
-    });
-});
-
+      const squareClientsMap = new Map()
+      res.forEach(vendor => {
+        squareClientsMap.set(
+          vendor.name,
+          new Client({
+            environment: Environment.Sandbox,
+            accessToken: vendor.squareInfo.accessToken
+          })
+        )
+      })
+      return resolve(squareClientsMap)
+    })
+    .catch(error => {
+      reject(error)
+    })
+})
 
 export default squareClients

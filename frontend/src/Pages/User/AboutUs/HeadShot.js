b89e0754a9e64835c6588eb725ce8fc2ed7b7efd
henryyqin <50307418+henryyqin@users.noreply.github.com>
2021-02-22 22:47:19 -0600

    Feat/about us styling (#146)
    * removed shadows, fixed text spacing
    
    * fixed title and back button spacing
    
    * updated goal title and text to mvp1 mock
    
    * project manager -> product manager, also fixed spacing for first column

--- frontend/src/Pages/User/AboutUs/HeadShot.js
+++ frontend/src/Pages/User/AboutUs/HeadShot.js
@@ -19,16 +19,21 @@ const HeadPhoto = styled.img`
 `
 
 const Name = styled.p`
+  font-family: Proxima Nova;
   font-weight: bold;
+  font-size: 2.5vh;
   margin: 0;
   margin-top: 15%;
-  padding: 0;
+  padding: 2vh 0;
   line-height: 90%;
 `
 
 const Position = styled.p`
+  font-family: Proxima Nova;
+  font-size: 2vh;
   margin: 0;
   padding: 0;
+  line-height: 90%;
 `
 
 const HeadShot = ({ src, name, position }) => {
--- frontend/src/Pages/User/AboutUs/index.js
+++ frontend/src/Pages/User/AboutUs/index.js
@@ -21,7 +21,7 @@ import Helena from '../../../images/Helena.jpeg'
 const MainDiv = styled.div`
   width: 100vw;
   display: grid;
-  grid-template-rows: 3% 15% 64%; //have to use max of 50% or user width? or maybe min
+  grid-template-rows: auto auto auto; //have to use max of 50% or user width? or maybe min
   text-align: center;
   align-items: center;
   justify-items: center;
@@ -31,48 +31,55 @@ const MainDiv = styled.div`
 
 const HeaderWrapper = styled.div`
   position: fixed;
-  height: 10vh;
-  font-size: 30px;
+  height: 11vh;
+  font-size: 4vh;
   width: 100vw;
   top: 0;
-  display: flex;
+  display: grid;
+  grid-template-columns: 10% 1fr 10%;
+  grid-template-areas: 'back text .';
   align-items: center;
+  justify-items: center;
   text-align: center;
-  justify-content: flex-start;
   z-index: 1;
   background-color: white;
-  padding-top: 1vh;
   box-shadow: 0px 0px 10px 3px rgba(0, 0, 0, 0.3);
 `
-const AboutText = styled.p`
+
+const BackSection = styled.div`
+  grid-area: back;
+  justify-self: start;
+  padding-left: 3vh;
+`
+
+const AboutText = styled.div`
   position: absolute;
+  grid-area: text;
   text-align: center;
-  justify-content: center;
-  top: 25%;
-  left: 47.5%;
-  transform: translate(-47.5%, 0);
+  font-family: Proxima Nova;
+  justify-self: center;
+  top: 20%;
   font-weight: bold;
 `
 const TitleLogoWrapper = styled.div`
   grid-row: 2/3;
   background-color: #faf6f2;
+  padding-top: 5vh;
   width: 100%;
-  height: 100%;
+  height: 40vh;
   text-align: center;
 `
 
 const MeetHedwigText = styled.p`
   position: relative;
   color: #f3725b;
-  font-size: 18pt;
+  font-family: Proxima Nova;
+  font-size: 3.5vh;
   text-align: center;
   line-height: 100%;
   font-weight: bold;
   top: 35%;
   height: 5vh;
-
-  text-shadow: 0px 15px 5px rgba(0, 0, 0, 0.1),
-    10px 20px 5px rgba(0, 0, 0, 0.05), -10px 20px 5px rgba(0, 0, 0, 0.05);
 `
 
 const HedwigLogo = styled.img`
@@ -87,13 +94,12 @@ const PhotoGrid = styled.div`
   background-color: #faf6f2;
   grid-row: 3/4;
   width: 100%;
-  height: 100%;
+  height: auto;
 
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
-  grid-template-rows: 1fr 1fr 1fr;
-  padding-left: 4%;
-  padding-right: 4%;
+  grid-template-rows: 1fr 1fr 1fr 1fr;
+  padding: 0vh 4% 2vh 4%;
 `
 const DescriptionBlock = styled.div`
   position: relative;
@@ -106,20 +112,18 @@ const DescriptionBlock = styled.div`
 const GoalTitle = styled.p`
   position: relative;
   color: #f3725b;
-  font-size: 18pt;
+  font-size: 4vh;
   text-align: center;
   line-height: 100%;
   font-weight: bold;
   margin: 2%;
-
-  text-shadow: 0px 15px 5px rgba(0, 0, 0, 0.1),
-    10px 20px 5px rgba(0, 0, 0, 0.05), -10px 20px 5px rgba(0, 0, 0, 0.05);
+  padding: 2vh 0vw 1vh 0vw;
 `
 const GoalText = styled.p`
   margin-left: 6%;
   margin-right: 6%;
   margin-top: 3%;
-  font-size: 12pt;
+  font-size: 2.2vh;
   text-align: justify;
 `
 
@@ -127,7 +131,8 @@ const ContactText = styled.p`
   margin-left: 6%;
   margin-right: 6%;
   margin-top: 0%;
-  font-size: 12pt;
+  font-size: 2.2vh;
+  padding-bottom: 2vh;
 `
 
 const AboutUs = () => {
@@ -136,16 +141,18 @@ const AboutUs = () => {
   return (
     <MainDiv>
       <HeaderWrapper>
-        <IoMdArrowRoundBack
-          onClick={() => navigate('/')}
-          style={{
-            position: 'relative',
-            fontSize: '25px',
-            cursor: 'pointer',
-            left: '1%'
-          }}
-        />
-        <AboutText>About the Creators</AboutText>
+        <BackSection>
+          <IoMdArrowRoundBack
+            onClick={() => navigate('/')}
+            style={{
+              position: 'relative',
+              fontSize: '4vh',
+              cursor: 'pointer',
+              left: '1%'
+            }}
+          />
+        </BackSection>
+        <AboutText>About Us</AboutText>
       </HeaderWrapper>
       <TitleLogoWrapper>
         <HedwigLogo src={hedwigLogo} />
@@ -155,12 +162,12 @@ const AboutUs = () => {
         <HeadShot
           src={Nikhita}
           name='Nikhita Gangla'
-          position='Project Manager'
+          position='Product Manager'
         />
         <HeadShot
           src={Melinda}
           name='Melinda Ding'
-          position='Project Manager'
+          position='Product Manager'
         />
         <HeadShot src={Victor} name='Victor Song' position='Team Lead' />
         <HeadShot src={Newton} name='Newton Huynh' position='Team Lead' />
@@ -174,18 +181,16 @@ const AboutUs = () => {
         <HeadShot src={Helena} name='Helena Hu' position='Developer' />
       </PhotoGrid>
       <DescriptionBlock>
-        <GoalTitle>What is Hedwig?</GoalTitle>
+        <GoalTitle>Our Goal</GoalTitle>
         <GoalText>
-          Hedwig is a web application built by RiceApps that enables members of
-          the Rice community to order food for pickup on campus from Rice
-          vendors such as Cohen House and East West Tea. In just a few clicks,
-          any member of the Rice community can place an order for pickup, pay
-          using credit card or Rice specific forms of payment such as Tetra or
-          Cohen House Club Card, and receive updates on its status in real-time.
-          Similarly, vendors can view and interact with orders in real-time,
-          with a comprehensive order management solution backed by a powerful
-          publicly-traded company. Hedwig currently only services breakfast and
-          lunch from Cohen House Curbside at the Club.
+        A revolutionary project designed from the ground up by Rice students for the Rice Community. 
+        In this challenging time, it is imperative that we take all necessary precautions as we begin 
+        to navigate life inside the “new normal” of the post COVID-19 era. That is why we have built Hedwig, 
+        a web experience which enables Rice students, faculty, and staff to engage with campus food 
+        establishments in a safe and frictionless manner. In just a few clicks, any member of the Rice 
+        community can place an order and receive updates on its status in real-time; similarly, vendors 
+        can view and interact with new orders in real-time, with a comprehensive order management solution 
+        backed by a powerful publicly-traded company.
         </GoalText>
         <GoalTitle>Contact us</GoalTitle>
         <ContactText>

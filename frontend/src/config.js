64693026683a298a10678f3a6062022a71aa3f82
Victor Song <vms2@rice.edu>
2021-02-25 15:26:40 -0600

    Actually use Square in development too

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -7,6 +7,6 @@ export const HEDWIG_VENDOR_PERMISSIONS = process.env.REACT_APP_HEDWIG_VENDOR_PER
 )
 export const SQUARE_CLIENT_ID = process.env.REACT_APP_SQUARE_CLIENT_ID
 export const SQUARE_CONNECTION_BASE_URL =
-  process.env.NODE_ENV === 'production'
+  process.env.REACT_APP_SQUARE_ENV === 'production'
     ? 'https://connect.squareup.com/'
     : 'https://connect.squareupsandbox.com/'

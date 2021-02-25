2f0f06d5cff0755a3e4a599e321eba1adad5c34a
Newton Huynh <nth8@rice.edu>
2021-02-24 22:24:24 -0600

    Fix missing mutation for pickupinstruction on set basic info

--- frontend/src/Pages/Vendor/VendorComponents/SetBasicInfoComponents/BasicInfoDashboard.js
+++ frontend/src/Pages/Vendor/VendorComponents/SetBasicInfoComponents/BasicInfoDashboard.js
@@ -373,7 +373,8 @@ function BasicInfoDashboard () {
         <Div warning>
           *These instructions are sent to the buyer when an order is submitted
         </Div>
-        <TextArea placeholder={placeholderInfo.pickupInstruction} />
+        <TextArea placeholder={placeholderInfo.pickupInstruction}
+        onChange={e => updateInfo({ pickupInstruction: e.target.value })} />
       </Div>
 
       <Div buttonwrapper disabled={isDisabled}>

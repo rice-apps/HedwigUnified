e37875076174805f83a943d7a78f0890104d6642
Henry Qin <hqin2400@gmail.com>
2021-02-28 13:40:41 -0600

    report bug page created

---
+++ frontend/src/components/ErrorBoundary.js
@@ -0,0 +1,26 @@
+import {React, Component} from 'react';
+
+class ErrorBoundary extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    console.log("Error in error boundary!");
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children; 
+  }
+}
+
+export default ErrorBoundary
---
+++ frontend/src/components/ReportBugPage.js
@@ -0,0 +1,48 @@
+import './components.css'
+import styled from 'styled-components/macro'
+import { useNavigate } from 'react-router-dom'
+
+const ReportPageButton = styled.button`
+  border-radius: 3vh;
+  height: 15vh;
+  width: 40vh;
+  border: none;
+  margin-bottom: 7vh;
+  font-size: 5vh;
+  background-color: #f3725b;
+  color: white;
+  font-family: Proxima Nova;
+  font-weight: bold;
+  :hover {
+    text-decoration: underline;
+    box-shadow: 0 0 50px rgba(33, 33, 33, 0.2);
+  }
+  :focus {
+    outline: none;
+  }
+`
+
+function ReportBugPage (props) {
+  const navigate = useNavigate()
+  const reportBug = () => {
+    window.open("https://forms.gle/QJgMDKxCoT19jHbF9", "_blank")
+  }
+  const goBack = () => {
+    navigate('/login')
+  }
+  return (
+    <div className='errorPage'>
+      <h1 style={{ 'font-size': '8vh' }}>
+        Uh oh! Something went wrong.
+      </h1>
+      <ReportPageButton onClick={reportBug} style={{ cursor: 'pointer' }}>
+        Report Bug
+      </ReportPageButton>
+      <ReportPageButton onClick={goBack} style={{ cursor: 'pointer' }}>
+        Back to Login
+      </ReportPageButton>
+    </div>
+  )
+}
+
+export default ReportBugPage
